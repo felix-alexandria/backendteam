fc5373940bd1e804aa52b9114221c369b91dde29
felix-alexandria <jesmiah@gmail.com>
2024-01-10 15:05:24 +0100

    todo route

--- mainApp.ts
+++ mainApp.ts
@@ -1,6 +1,8 @@
 import { Application, Request, Response } from "express"
+import todo from "./Router/TodoRouter"
 export const mainApp = (app: Application) => {
     try {
+        app.use("/", todo)
         app.get("/", (req: Request, res: Response) => {
             try {
                 return res.status(200).json({
